8951d271424a2a46d7321d073a8a9a054ea87bdb
Noah S <noahalexsimon@outlook.de>
2021-12-26 23:51:52 +0100

    Added socials, jobs, welcome message

--- custom-components.js
+++ custom-components.js
@@ -1,7 +1,7 @@
 
 Vue.component('carousel', {
     props: ['title', 'info', 'img', 'active'],
-    template: `<div :class="'carousel-item' + (active ? ' active' : '')">
+    template: `<div :class="'col-10 carousel-item' + (active ? ' active' : '')">
                     <img class="d-block w-100" v-bind:src="img" v-bind:alt="title">
                     <div class="carousel-caption">
                       <h3>{{ info }}</h3>
@@ -9,12 +9,35 @@ Vue.component('carousel', {
                     </div>
                   </div>`
 })
-Vue.component('job-card', {
-    props: [],
-    template: `
-        <div>
-        
-        </div>
-    `
-})
 new Vue({el: '#c_showcase'});
+new Vue({
+    el: '#cards',
+    data: {
+        positions: [{name: "Writer", tag: "Few",
+                    description: "We don't want to just change the visual style. Transferring something into " +
+                        "minecraft needs more than that. What exactly does that mean? That's on you."},
+                    {name: "Blockbuster Expert", tag: "Few",
+                        description: "You'll decorate sets with extras and model blocks with the help of " +
+                            "the blockbuster mod. With this great creative freedom comes great responsibility."},
+                    {name: "Modeller", tag: "",
+                        description: "We don't want to just change the visual style. Transferring something into " +
+                            "minecraft needs more than that. What exactly does that mean? That's on you."},
+                    {name: "VFX Artist/Editor",
+                        description: "Even though our builders and _____ try their best to build an immersive world" +
+                            " we need to add even more in post to finish of a video."},
+                    {name: "Builder",
+                        description: "Our building department recreates entire worlds block-by-block. " +
+                            "Show us your creativity and become a CinemaCraft Builder."},
+                    {name: "Voice Actor",
+                        description: "In order to transfer star wars into minecraft, we'll have to change up the script. " +
+                            "You'll be the voice of this script."},
+                    {name: "Developer",
+                        description: "Not only do we need people to maintain our Website and Discord Bot, but also " +
+                            "do we need - as a Film and FX Studio - some internal tools."}]
+    }
+})
+
+const width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+if(width > 900) window.scrollTo(0, 100)
